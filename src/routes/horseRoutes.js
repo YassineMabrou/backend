78018e4ce30d091a35e772adbd436ec55e844c6d
YassineMabrou <yassinemabrouk204@gmail.com>
2025-05-20 00:25:18 +0100

    z

--- src/routes/horseRoutes.js
+++ src/routes/horseRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const HorseController = require('../controllers/horseController');
+const HorseController = require('../controllers/HorseController');
 const Horse = require('../models/Horse');
 const User = require('../models/userModel');
 const mongoose = require('mongoose');
