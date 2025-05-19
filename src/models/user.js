c1530560d28056cf23fa523d3cfe8039c4d36d25
YassineMabrou <yassinemabrouk204@gmail.com>
2025-05-19 22:11:55 +0100

    update

--- src/models/user.js
+++ src/models/user.js
@@ -5,16 +5,16 @@ const userPermissionSchema = new mongoose.Schema({
   manage_action: { type: Boolean, default: false },
   manage_qualification: { type: Boolean, default: false },
   manage_movement: { type: Boolean, default: false },
-  manage_categories: { type: Boolean, default: false },
   manage_location: { type: Boolean, default: false },
   manage_contact: { type: Boolean, default: false },
 }, { _id: false });
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true }, // ✅ username back
+  name: { type: String, required: true },
+  username: { type: String },
   email: { type: String, required: true, unique: true },
+  phone: { type: String },
   password: { type: String, required: true },
-  role: { type: String, default: "user" },
   permissions: userPermissionSchema,
 }, { timestamps: true });
 
